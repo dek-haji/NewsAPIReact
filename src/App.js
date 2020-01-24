b2c760fa09c486457668cf8b470c7c73af81a63e
dek-haji <deeeqhaji@gmail.com>
2020-01-24 17:40:00 -0600

    now i loaded the data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      isLoaded: false,
+    }
+   // this.getQuotes = this this.getQuotes.bind(this);
+  }
+  componentDidMount() {
+    fetch("https://api-v2.intrinio.com/companies/AAPL?api_key=OjEzYzBiMGM5NmU4NzZiNzFkN2ZkMzNkNWEzYjc5MjRl").then(response => response.json())
+        .then(text1 => {
+          this.setState({
+            isLoaded: true,
+            quotes: text1,
+          })
+        });
+  }
+  render() {
+    console.log(this.state.quotes)
+    const { isLoaded, quotes } = this.state;
+    if (!isLoaded) {
+      return <div>Loading....</div>;
+    }
+    else {
+      return (
+        <div className="App">
+          <ul>
+            {quotes.map(item => (
+              <li key={item.id}>
+                
+                  { item.short_description }
+                
+              </li>
+            ))};
+        </ul>
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
